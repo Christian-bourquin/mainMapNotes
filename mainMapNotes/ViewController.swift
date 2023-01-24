57ebbb8b92541bacdcf16b48f63b5242dc0e204d
Christian-bourquin <christian.bourquin23@d155.org>
2023-01-24 08:04:57 -0600

    do tonight

--- mainMapNotes/ViewController.swift
+++ mainMapNotes/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    @IBOutlet weak var mapOutlet: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    @IBAction func zoomAction(_ sender: Any) {
+        
+        
+    }
+    
+    
+    @IBAction func searchButton(_ sender: Any) {
+        //checking if there is a response
+        
+        for dog in response.mapitems{
+            self/spots.append(mapotems)
+            let anotation = MKPointAnnotation()//build a blank annotation
+            annotation.quardenite = mapitem.placemark.coordinate
+        
+            annotation.title = mapItem.placemark.name
+            mapOutlet.addAnnotation(annotation)
+        }
+    }
+    
 }
 
